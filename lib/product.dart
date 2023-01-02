ba9287dd74c82c4ce16da469fe500d234f19b3d4
mzaeemnasir <m.zaeem.nasir@gmail.com>
2023-01-02 23:38:36 +0500

    added product.dart

--- lib/product.dart
+++ lib/product.dart
@@ -1,15 +1,27 @@
+import 'dart:convert';
+
+import 'package:firedart/firestore/firestore.dart';
+import 'package:firedart/firestore/models.dart';
 import 'package:searchaholic/imports.dart';
 import 'package:searchaholic/sidebar.dart';
 
 // Product class to Show the Product Details
-
 class Product extends StatefulWidget {
   const Product({Key? key}) : super(key: key);
+
   @override
   _Product createState() => _Product();
 }
 
 class _Product extends State<Product> {
+  List<Object> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,5 +53,42 @@ class _Product extends State<Product> {
         ],
       ),
     );
+  }
+
+  Future getProducts() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path;
+    Directory folder = Directory('$path/SeachAHolic');
+
+    // getting the email from the user.json file
+    File file = File('$path/SeachAHolic/user.json');
+
+    try {
+      String email = jsonDecode(file.readAsStringSync())['email'];
+
+      // Getting Documents from Firestore
+      var data = Firestore.instance.collection(email).document("Product");
+      var snapshot = await data.get();
+    } catch (e) {
+      print(e);
+      // Alert Dialog to Show the Error
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Error"),
+              content: Text("Something went wrong"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("OK"))
+              ],
+            );
+          });
+    }
+
+    // Getting Documents from Firestore again the email
   }
 }
